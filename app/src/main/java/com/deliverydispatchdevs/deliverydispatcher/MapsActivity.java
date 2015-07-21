2ef6ab6d204150e4d43bbb46c03b9e6308e033c5
Ben Reeves <benwolverine2019@gmail.com>
2015-07-20 20:39:59 -0500

    Changed to getMapAsync()
    Retrieve the GoogleMap from the MapFragment with getMapAsync() rather than getMap()

--- app/src/main/java/com/deliverydispatchdevs/deliverydispatcher/MapsActivity.java
+++ app/src/main/java/com/deliverydispatchdevs/deliverydispatcher/MapsActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -49,13 +50,23 @@ public class MapsActivity extends FragmentActivity
         if (mMap == null)
         {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null)
-            {
-                setUpMap();
-            }
+            // Use getMapAsync() is the recommended way to get a map.
+            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMapAsync(new OnMapReadyCallback()
+                    {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap)
+                        {
+                            mMap = googleMap;
+
+                            // Check if we were successful in obtaining the map.
+                            if (mMap != null)
+                            {
+                                setUpMap();
+                            }
+                        }
+                    });
+
         }
     }
 
