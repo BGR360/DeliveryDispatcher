1c48416272d69bb95b1b92a52fad5257b3f01a85
Ben Reeves <benwolverine2019@gmail.com>
2015-07-22 18:51:51 -0500

    Added SearchViewManager class
    This class handles all the setup of the collapsible SearchView that's held in MapActivity's ActionBar

---
+++ app/src/main/java/com/deliverydispatchdevs/deliverydispatcher/SearchViewManager.java
@@ -0,0 +1,10 @@
+package com.deliverydispatchdevs.deliverydispatcher;
+
+/**
+ * Created by BGR on 7/22/2015.
+ *
+ * This class handles all the setup of the collapsible SearchView that's held in MapActivity's ActionBar
+ */
+public class SearchViewManager
+{
+}
