a89a6ccbdfc726da5dfd426ed39759b5b63ca39d
Saad <saadchoudry2014@gmail.com>
2015-07-27 16:42:10 -0500

    Testing OrdersView

--- app/src/main/java/com/deliverydispatchdevs/deliverydispatcher/PendingOrders.java
+++ app/src/main/java/com/deliverydispatchdevs/deliverydispatcher/PendingOrders.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class PendingOrders extends AppCompatActivity {
 
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
